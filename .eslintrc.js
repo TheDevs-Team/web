f8a66e46907d73493f6259d59a1e6d3a588306d2
eduardozuppodev <zuppo.edu@gmail.com>
2021-08-23 12:41:10 -0300

    init commit configs

---
+++ .eslintrc.js
@@ -0,0 +1,71 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      jest: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:prettier/recommended',
+      'plugin:import/recommended',
+      'plugin:import/errors',
+      'plugin:import/typescript',
+      'plugin:import/warnings',
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint', 'eslint-plugin-import-helpers'],
+    settings: {
+      'import/resolver': {
+        typescript: {},
+        'babel-plugin-root-import': {
+          rootPathPrefix: '~',
+          rootPathSuffix: 'src',
+        },
+      },
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'prettier/prettier': ['error'],
+      'react/react-in-jsx-scope': 'off',
+      'no-undef': 'off',
+      'no-unused-vars': 'error',
+      'react/prop-types': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      'import-helpers/order-imports': [
+        'off',
+        {
+          newlinesBetween: 'always',
+          groups: ['/^@shared/', 'module', ['parent', 'sibling', 'index']],
+          alphabetize: { order: 'desc', ignoreCase: true },
+        },
+      ],
+      'react-hooks/exhaustive-deps': 'off',
+      'import/no-unresolved': 'off',
+      'import/namespace': 'off',
+      'import/export': 'off',
+      'import/default': 'off',
+      'import/no-duplicates': 'off',
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  };
+  
