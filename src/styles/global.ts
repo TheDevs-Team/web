9a2a384c7083e291363d0bf94333fa1c842334a9
eduardozuppodev <zuppo.edu@gmail.com>
2021-08-23 12:49:52 -0300

    add globalstyles

---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', 'sans-serif';
+    overflow-x: hidden;
+    text-decoration: none;
+    outline: none;
+
+  }
+
+  html{
+    display: flex;
+    flex-direction: column;
+    display: center;
+    justify-content: center;
+  }
+
+
+`;
