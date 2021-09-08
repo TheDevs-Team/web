2870d636d47d2c105949a90ec9ce9fc4b758cee8
eduardozuppodev <zuppo.edu@gmail.com>
2021-09-08 12:47:59 -0300

    feat: add localstorage and notify

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState } from 'react';
 import GlobalStyle from '~/styles/global';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import logoImg from 'assets/logo.png';
 import Input from '~/components/Input/Input';
 import { Container, Nav, FormContainer, Header, Title, Button, Span } from './styles';
-import logoImg from 'assets/logo.png';
+
+import api from '~/api/user.api';
 
 export const Login: React.FC = () => {
+  const [password, setPassword] = useState<string>();
+  const [email, setEmail] = useState<string>();
+
+  const notify = (value) => toast.error(value);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await api.Login({ email, password });
+
+    if (response) {
+      await localStorage.setItem('@g2k:token', response.data.token);
+
+      return (window.location.href = 'www.google.com');
+    }
+    return notify('Email/Senha Incorretos!');
+  };
+
   return (
     <>
       <GlobalStyle />
@@ -13,13 +36,14 @@ export const Login: React.FC = () => {
         <Nav>
           <img src={logoImg} />
         </Nav>
-        <FormContainer>
+        <FormContainer onSubmit={(event) => handleSubmit(event)}>
           <Header>
             <Title>Faça seu login</Title>
           </Header>
-          <Input placeholder="E-mail" type="text" />
-          <Input placeholder="Senha" type="password" />
+          <Input placeholder="E-mail" type="text" onChange={(event) => setEmail(event.target.value)} />
+          <Input placeholder="Senha" type="password" onChange={(event) => setPassword(event.target.value)} />
           <Button>CONTINUAR</Button>
+          <ToastContainer />
           <Span>Ainda não sou G2K </Span>
         </FormContainer>
       </Container>
