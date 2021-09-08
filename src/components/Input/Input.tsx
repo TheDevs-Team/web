d5523a37db69de0223d18f40be647e5a1bd97560
eduardozuppodev <zuppo.edu@gmail.com>
2021-09-08 12:48:16 -0300

    feat: add ...rest

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 
 import { Container } from './styles';
@@ -5,10 +6,11 @@ import { Container } from './styles';
 type Props = {
   placeholder: string;
   type: string;
+  onChange?: (event) => void;
 };
 
-const Input: React.FC<Props> = ({ placeholder, type }) => {
-  return <Container placeholder={placeholder} type={type} />;
+const Input: React.FC<Props> = ({ placeholder, type, onChange, ...rest }) => {
+  return <Container {...{ ...rest }} placeholder={placeholder} type={type} onChange={onChange} />;
 };
 
 export default Input;
