23b694deface6b385af1e002e962f46fb8d51cb0
eduardozuppodev <zuppo.edu@gmail.com>
2021-09-10 12:59:53 -0300

    fix: getToken

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,16 @@
 import axios from 'axios';
+import { getToken } from '~/utils';
 
 const api = axios.create({
   baseURL: 'http://172.15.0.31:3333',
-  headers: {
-    'Content-type': 'application/json',
-  },
+});
+
+api.interceptors.request.use(async (config) => {
+  const token = getToken();
+  if (token) {
+    config.headers.authorization = token;
+  }
+  return config;
 });
 
 export default api;
