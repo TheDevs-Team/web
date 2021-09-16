c70dd5fa4e1c76df3daa6747415e959ae3d53cd0
eduardozuppodev <zuppo.edu@gmail.com>
2021-09-16 11:06:08 -0300

    feat: create aside

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,6 @@
 export * from './CourseCard/CourseCard';
 export * from './Input/Input';
 export * from './Modal/Modal';
+export * from './Header/Header';
+export * from './Aside/Aside';
+export * from './Main/Main';
