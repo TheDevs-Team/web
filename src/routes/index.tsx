a15a894dbf8a42403373b1de6201d16e9cbb4815
eduardozuppodev <zuppo.edu@gmail.com>
2021-09-16 11:07:39 -0300

    feat: update exports

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import React from 'react';
 import { PrivateRoute } from './PrivateRoutes';
-import { Login, Course } from '../pages';
+import { Login, Course, HomeAdm } from '../pages';
 
 export const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
+      <Route exact path={'/adm'} component={HomeAdm} />
       <Route exact path={'/entrar'} component={Login} />
       <PrivateRoute exact path={'/'} component={Course} />
       <PrivateRoute
