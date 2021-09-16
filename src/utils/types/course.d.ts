4e9770cbd9fa7303cd04b89259a4770cff629672
gustavo-evaristo <gug.henri1@gmail.com>
2021-09-16 17:17:27 -0300

    feat: course type

--- src/utils/types/course.d.ts
+++ src/utils/types/course.d.ts
@@ -1,9 +1,20 @@
 declare type CourseType = {
   id: string;
+  active: boolean;
   name: string;
   description: string;
   manager_id: string;
-  active: boolean;
   created_at: Date;
   updated_at: Date;
+  user: UserType;
+};
+
+declare type CreateCourseType = {
+  name: string;
+  description: string;
+};
+
+declare type UpdateCourseType = {
+  id: string;
+  name: string;
 };
