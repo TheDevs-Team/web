144030d5d2efb8bdd8227dbf7bfbdc63371831cb
eduardozuppodev <zuppo.edu@gmail.com>
2021-09-24 12:13:36 -0300

    fix: update login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-
 import { Input } from '~/components';
 import { Container, Nav, FormContainer, Header, Title, Button, Span, Logo } from './styles';
 
