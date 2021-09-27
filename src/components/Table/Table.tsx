66a09ba766a4489ae87e4cac515cace75aee679a
eduardozuppodev <zuppo.edu@gmail.com>
2021-09-27 10:20:00 -0300

    feat: add modal

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal } from '../../components';
 
 import { Container, Thead, Tr, Th, Td, Tbody, ButtonEdit, ButtonRemove, ButtonCreate } from './styles';
 
@@ -7,40 +8,44 @@ type Props = {
 };
 
 export const Table: React.FC<Props> = ({ users }) => {
+  const [modal, setModal] = useState(false);
+
   return (
-    <Container>
-      <Thead>
-        <Tr>
-          <Th>Usuários</Th>
-          <Th scope="row">Nome</Th>
-          <Th scope="row">Documento</Th>
-          <Th scope="row">E-mail</Th>
-          <Th scope="row">Financeiro</Th>
-          <Th scope="row">Status</Th>
-          <Th scope="row"></Th>
-          <Th scope="row">
-            <ButtonCreate>Adicionar</ButtonCreate>
-          </Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {users?.map((user: UserType, idx: number) => (
-          <Tr key={idx}>
-            <Th scope="row">{idx + 1}</Th>
-            <Td>{user.name}</Td>
-            <Td>{user.document}</Td>
-            <Td>{user.email}</Td>
-            <Td>{user.financial_status}</Td>
-            <Td>{user.active}</Td>
-            <Td>
-              <ButtonEdit>Editar</ButtonEdit>
-            </Td>
-            <Td>
-              <ButtonRemove>X</ButtonRemove>
-            </Td>
+    <>
+      {modal && <Modal onClose={() => setModal(false)} />}
+      <Container>
+        <Thead>
+          <Tr>
+            <Th>Usuários</Th>
+            <Th scope="row">Nome</Th>
+            <Th scope="row">Documento</Th>
+            <Th scope="row">E-mail</Th>
+            <Th scope="row">Financeiro</Th>
+            <Th scope="row">Status</Th>
+            <Th scope="row"></Th>
+            <Th scope="row">
+              <ButtonCreate onClick={() => setModal(true)}>Adicionar</ButtonCreate>
+            </Th>
           </Tr>
-        ))}
-        {/* <Tr>
+        </Thead>
+        <Tbody>
+          {users?.map((user: UserType, idx: number) => (
+            <Tr key={idx}>
+              <Th scope="row">{idx + 1}</Th>
+              <Td>{user.name}</Td>
+              <Td>{user.document}</Td>
+              <Td>{user.email}</Td>
+              <Td>{user.financial_status}</Td>
+              <Td>{user.active}</Td>
+              <Td>
+                <ButtonEdit>Editar</ButtonEdit>
+              </Td>
+              <Td>
+                <ButtonRemove>X</ButtonRemove>
+              </Td>
+            </Tr>
+          ))}
+          {/* <Tr>
           <Th scope="row">1</Th>
           <Td>Eduardo Alves Zuppo</Td>
           <Td>523.029.008-04</Td>
@@ -194,7 +199,8 @@ export const Table: React.FC<Props> = ({ users }) => {
             <ButtonRemove>X</ButtonRemove>
           </Td>
         </Tr> */}
-      </Tbody>
-    </Container>
+        </Tbody>
+      </Container>
+    </>
   );
 };
