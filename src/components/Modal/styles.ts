e14255b9c1146206082fca5605de8be1754b3f65
eduardozuppodev <zuppo.edu@gmail.com>
2021-10-04 10:55:04 -0300

    feat: add styles

--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { colors } from '~/theme';
 
 export const Container = styled.div`
   width: 100%;
@@ -16,7 +17,7 @@ export const Container = styled.div`
 
 export const Content = styled.div`
   width: 450px;
-  background: #222222;
+  background: ${colors.primary.dark};
   border-radius: 10px;
   display: flex;
   flex-direction: column;
@@ -33,7 +34,7 @@ export const CloseContainer = styled.div`
   justify-content: flex-end;
   padding-right: 10px;
 
-  color: #fff;
+  color: ${colors.primary.white};
 `;
 
 export const Form = styled.form`
@@ -50,27 +51,28 @@ export const InputContainer = styled.div`
 
 export const Label = styled.label`
   font-size: 14px;
-  color: #cecece;
+  color: ${colors.primary.light};
 `;
 
 export const Input = styled.input`
   width: 95%;
 
-  border: 1px solid #2b2b2c;
+  border: 1px solid ${colors.primary.main};
   background: transparent;
   border-radius: 10px;
   padding: 10px 0 10px 10px;
   outline: 0;
-  color: #c4c4c4;
+  color: ${colors.primary.grey};
 `;
 
 export const Btn = styled.button`
-  background: #217150;
-  color: #fff;
+  background: ${colors.primary.green};
+  color: ${colors.primary.white};
   font-size: 16px;
   font-weight: 300;
   width: 60%;
   height: 40px;
+  margin: 10px 0;
   border: none;
   border-radius: 15px;
   opacity: 0.8;
@@ -84,14 +86,57 @@ export const Btn = styled.button`
 export const Select = styled.select`
   width: 98%;
 
-  border: 1px solid #2b2b2c;
+  border: 1px solid ${colors.primary.main};
   background: transparent;
   border-radius: 10px;
   padding: 10px 0 10px 10px;
   outline: 0;
-  color: #c4c4c4;
+  color: ${colors.primary.grey};
 `;
 
 export const Option = styled.option`
-  background: #2b2b2c;
+  background: ${colors.primary.main};
+`;
+
+// modal remove
+export const Remove = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const OptionRemove = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+
+  padding: 15px 0;
+`;
+
+export const RemoveBtn = styled.button`
+  background: transparent;
+  border: 1px solid ${colors.primary.white};
+  color: ${colors.primary.white};
+  font-size: 15px;
+  font-weight: 300;
+  width: 40%;
+  height: 40px;
+  border-radius: 15px;
+  opacity: 0.8;
+  cursor: pointer;
+  &:hover {
+    opacity: 1;
+    transition: 0.4s;
+  }
+`;
+
+export const TitleRemove = styled.span`
+  color: ${colors.primary.white};
+  font-weight: 300;
+  font-size: 15px;
 `;
