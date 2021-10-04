5820e15788d87f7e4a9620648aed3a65b088c907
eduardozuppodev <zuppo.edu@gmail.com>
2021-10-04 13:49:13 -0300

    feat: add course component

---
+++ src/pages/Course/Course.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import GlobalStyles from '../../styles/global';
+import { Container, Main, CourseField, Courses } from './styles';
+import { Header, Table } from '~/components';
+
+const Course: React.FC = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Container>
+        <Header />
+        <Main>
+          <CourseField>
+            <Courses>
+              <Table />
+            </Courses>
+          </CourseField>
+        </Main>
+      </Container>
+    </>
+  );
+};
+
+export default Course;
