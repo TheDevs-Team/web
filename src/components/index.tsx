a8f8005ae341c340a9a72e0015576866b2d0cfc3
eduardozuppodev <zuppo.edu@gmail.com>
2021-10-04 10:55:21 -0300

    fix: update imports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,5 @@
 export * from './Input';
-export * from './Input';
 export * from './Header/Header';
 export * from './Table/Table';
-export * from './Modal/Modal';
+export * from './If';
+export { default as Modal } from './Modal/Modal';
