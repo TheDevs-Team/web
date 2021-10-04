3e0b4d1d969724ce2c271f21625f5e3c7651cb1d
eduardozuppodev <zuppo.edu@gmail.com>
2021-10-04 13:49:48 -0300

    feat: add export

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
 export { default as Login } from './Login';
 export { default as HomeAdm } from './HomeAdm';
 export { default as Profile } from './Profile/Profile';
+export { default as Course } from './Course/Course';
