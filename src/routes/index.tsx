06fe95ecb474460837aa8ebaf86c3da529d5ff84
eduardozuppodev <zuppo.edu@gmail.com>
2021-10-04 13:48:49 -0300

    feat: add route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import React from 'react';
-import { Login, HomeAdm, Profile } from '../pages';
+import { Login, HomeAdm, Profile, Course } from '../pages';
 import { PrivateRoute } from './PrivateRoutes';
 
 export const Routes: React.FC = () => (
@@ -8,6 +8,7 @@ export const Routes: React.FC = () => (
     <Switch>
       <PrivateRoute exact path={'/'} component={HomeAdm} />
       <PrivateRoute exact path={'/perfil'} component={Profile} />
+      <PrivateRoute exact path={'/cursos'} component={Course} />
       <Route exact path={'/entrar'} component={Login} />
     </Switch>
   </BrowserRouter>
