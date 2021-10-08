62902bd933a3e44958941430fd506a7109891cea
gustavo-evaristo <gug.henri1@gmail.com>
2021-10-08 12:08:41 -0300

    refactor: api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from '~/utils';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://g2k-api.herokuapp.com',
 });
 
 api.interceptors.request.use(async (config) => {
