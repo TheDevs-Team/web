5d486c9f5beb5a49b460e88e482d5bdbd3a5df2e
gustavo-evaristo <gug.henri1@gmail.com>
2021-10-19 12:00:48 -0300

    refactor: component

--- src/components/Recharts/styles.ts
+++ src/components/Recharts/styles.ts
@@ -1,13 +1,12 @@
 import styled from 'styled-components';
 import { AreaChart, ResponsiveContainer } from 'recharts';
 
-export const Container = styled(ResponsiveContainer)`
-  width: 98%;
-  height: 92%;
-`;
+export const Container = styled(ResponsiveContainer).attrs({
+  width: '98%',
+  height: '95%',
+})``;
 
 export const AreaChartStyled = styled(AreaChart)`
-  width: 730;
-  height: 250;
-  margin: 10 30 0 0;
+  width: 700px;
+  height: 250px;
 `;
