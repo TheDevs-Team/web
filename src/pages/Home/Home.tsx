3e8d3df301c9f7ed1bb394604caf75f563003815
gustavo-evaristo <gug.henri1@gmail.com>
2021-10-19 12:04:41 -0300

    fix: errors

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -159,28 +159,6 @@ const Home: React.FC<Props> = ({ hover, setHover }) => {
         </InfoCard>
 
         <ChartsArea>
-          {/* <ResponsiveContainer width="98%" height="92%">
-            <AreaChart width={730} height={250} data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-              <defs>
-                <linearGradient id="coloruv" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#67f7bd" stopOpacity={0.8} />
-                  <stop offset="95%" stopColor="#363333" stopOpacity={0} />
-                </linearGradient>
-              </defs>
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Area
-                type="monotone"
-                dataKey="alunos"
-                stroke="#67f7bd"
-                fillOpacity={1}
-                fill="url(#coloruv)"
-                strokeWidth={5}
-              />
-            </AreaChart>
-          </ResponsiveContainer> */}
-
           <Recharts data={data} />
         </ChartsArea>
       </Main>
