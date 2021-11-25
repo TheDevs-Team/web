2baf0aa185b3daff58c5165a57d0b6b62a29db29
Eduardo Zuppo <eduardozuppo@MacBook-Air-de-Eduardo.local>
2021-11-25 10:57:22 -0300

    fix: routing

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -2,14 +2,30 @@ import { inject, observer } from 'mobx-react';
 import React from 'react';
 import Profile from './Profile';
 import { UserStore } from 'store';
+import { notify } from '~/utils';
+import { useHistory } from 'react-router-dom';
 
 type Props = {
   user: UserStore;
 };
 
 const ProfileContainer: React.FC<Props> = ({ user }) => {
-  const submitForm = (state: any) => {
-    user.update({ ...state, id: user.profile.id, document: user.profile.document, type: user.profile.type });
+  const history = useHistory();
+
+  const submitForm = async (state: any) => {
+    const response = await user.update({
+      ...state,
+      id: user.profile.id,
+      document: user.profile.document,
+      type: user.profile.type,
+    });
+
+    if (response) {
+      notify('success', 'Sucesso ao atualizar dados!');
+      return history.push('/');
+    }
+
+    return notify('error', 'Erro ao atualizar dados!');
   };
   return <Profile submitForm={submitForm} />;
 };
